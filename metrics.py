ba552389106b2f829ee7545fab239aba967fc78a
luongbs01 <luongbs01@gmail.com>
2022-07-02 16:46:53 +0700

    Fix pred_opinion, metrics

--- metrics.py
+++ metrics.py
@@ -5,11 +5,11 @@ from sklearn.metrics import recall_score
 
 y_true = [-1, 1, 1, -1, 1, -1, 1, 1, 1, 1]
 y_pred = [-1, 1, 1, -1, -1, -1, 1, 1, 1, -1]
-#print accuracy_score(y_true, y_pred)
-#print precision_score(y_true, y_pred, average='macro') 
+# print(accuracy_score(y_true, y_pred))
+# print(precision_score(y_true, y_pred, average='macro'))
 
 feature_true = [1,-1,-1,1,1,1,-1,1,1,1,1,1,1,1]
 feature_pred = [1,-1,-1,1,1,1,1,-1,1,1,-1,1,1,1]
 
 
-print accuracy_score(feature_true, feature_pred)
+print(accuracy_score(feature_true, feature_pred))
