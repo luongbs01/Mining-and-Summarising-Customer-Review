591b9b5ed50a57bc6dc3de55c61752e7c3c19853
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2022-07-21 15:23:15 +0700

    Fix code

--- final_code.py
+++ final_code.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import nltk
-import os, sys
-
+import os
+import sys
 from nltk.corpus import stopwords
 import numpy as np
 import collections
@@ -12,82 +12,86 @@ lemma = nltk.WordNetLemmatizer()
 cachedstopwords = stopwords.words("english")
 
 
-
 def mult_token(review):
-	final = []
-	sent_text = nltk.sent_tokenize(review)
-	#print sent_text
-	for sentence in sent_text:
-		tokenized_text = nltk.word_tokenize(sentence)
-		tagged = nltk.pos_tag(tokenized_text)
-		#print(tagged)
-		final.append(tagged)
-		#print(final)
-	return final
+    final = []
+    sent_text = nltk.sent_tokenize(review)
+    # print('sent_text', sent_text)
+    for sentence in sent_text:
+        tokenized_text = nltk.word_tokenize(sentence)
+        tagged = nltk.pos_tag(tokenized_text)
+        # print('tagged', tagged)
+        final.append(tagged)
+        # print('final', final)
+    return final
+
 
 def transaction(arr):
-	tmp = []
-	bit = []
-	h, w, n = 0, 0, len(arr)
-	for i in range(0, n):
-		bit.append(0)
-		m, w = len(arr[i]), 0
-		for j in range(0, m):
-			if arr[i][j][1] == "NN" or arr[i][j][1] == "NNS" or arr[i][j][1] == "NNP" or arr[i][j][1] == "NNPS":
-				if w == 0:
-					tmp.append([])
-				tmp[h].append(str(arr[i][j][0]))
-				w += 1
-				bit[i] += 1
-		if w >= 1:
-			h += 1
-	return tmp, bit
+    tmp = []
+    bit = []
+    h, w, n = 0, 0, len(arr)
+    for i in range(0, n):
+        bit.append(0)
+        m, w = len(arr[i]), 0
+        for j in range(0, m):
+            if arr[i][j][1] == "NN" or arr[i][j][1] == "NNS" or arr[i][j][1] == "NNP" or arr[i][j][1] == "NNPS":
+                if w == 0:
+                    tmp.append([])
+                tmp[h].append(str(arr[i][j][0]))
+                w += 1
+                bit[i] += 1
+        if w >= 1:
+            h += 1
+    return tmp, bit
+
 
 def cntadj(arr):
-	tmp = []
-	bit = []
-	h, w, n = 0, 0, len(arr)
-	for i in range(0, n):
-		bit.append(0)
-		m, w = len(arr[i]), 0
-		for j in range(0, m):
-			if arr[i][j][1] == "JJ" or arr[i][j][1] == "JJS" or arr[i][j][1] == "JJR":
-				if w == 0:
-					tmp.append([])
-				tmp[h].append(str(arr[i][j][0]))
-				w += 1
-				bit[i] += 1
-		if w >= 1:
-			h += 1
-	return tmp, bit
+    tmp = []
+    bit = []
+    h, w, n = 0, 0, len(arr)
+    for i in range(0, n):
+        bit.append(0)
+        m, w = len(arr[i]), 0
+        for j in range(0, m):
+            if arr[i][j][1] == "JJ" or arr[i][j][1] == "JJS" or arr[i][j][1] == "JJR":
+                if w == 0:
+                    tmp.append([])
+                tmp[h].append(str(arr[i][j][0]))
+                w += 1
+                bit[i] += 1
+        if w >= 1:
+            h += 1
+    return tmp, bit
+
 
 def rem_stop_word(arr, bit):
-	tmp = []
-	h, w, fl, n = 0, 0, 0, len(arr)
-	for i in range(0, n):
-		while bit[fl] == 0:
-			fl += 1
-		m, w = len(arr[i]), 0
-		for j in range(0, m):
-			if arr[i][j] not in cachedstopwords:
-				if w == 0:
-					tmp.append([])
-				tmp[h].append(str(arr[i][j]))
-				w += 1
-			else:
-				bit[fl] -= 1
-		if w >= 1:
-			h += 1
-		fl += 1
-	return tmp, bit
+    tmp = []
+    h, w, fl, n = 0, 0, 0, len(arr)
+    for i in range(0, n):
+        while bit[fl] == 0:
+            fl += 1
+        m, w = len(arr[i]), 0
+        for j in range(0, m):
+            if arr[i][j] not in cachedstopwords:
+                if w == 0:
+                    tmp.append([])
+                tmp[h].append(str(arr[i][j]))
+                w += 1
+            else:
+                bit[fl] -= 1
+        if w >= 1:
+            h += 1
+        fl += 1
+    return tmp, bit
+
 
 def lemm(arr):
-	n = len(arr)
-	for i in range(0, n):
-		m = len(arr[i])
-		for j in range(0, m):
-			arr[i][j] = lemma.lemmatize(arr[i][j])
-	return arr
+    n = len(arr)
+    for i in range(0, n):
+        m = len(arr[i])
+        for j in range(0, m):
+            arr[i][j] = lemma.lemmatize(arr[i][j])
+    return arr
+
 
 def convert1d(arr):
     ll = len(transaction)
@@ -98,12 +102,14 @@ def convert1d(arr):
             tmp.append(transaction[i][j])
     return tmp
 
+
 def freqone(seed, arr):
     tmp = []
     for var in seed:
         if arr.count(var) >= support:
             tmp.append(var)
     return tmp
+
 
 def createdct(arr):
     dct = {}
@@ -114,9 +120,11 @@ def createdct(arr):
         dct2[i+1] = arr[i]
     return dct, dct2
 
+
 def crtscndmat(i):
     mat = [[0 for x in range(i+1)] for y in range(i+1)]
     return mat
+
 
 def freq2(rev, dct, dct2):
     tmp, ll = [], len(rev)
@@ -138,10 +146,11 @@ def freq2(rev, dct, dct2):
     for i in range(1, ll):
         for j in range(i+1, ll):
             if pair[i][j] >= support:
-                ans[num].append((dct2[i],dct2[j]))
+                ans[num].append((dct2[i], dct2[j]))
                 ans.append([])
                 num += 1
     return ans
+
 
 def usefuladj(feature, featcnt, adject, adjcnt, frstfreq):
     ll, rr, j = len(featcnt), len(feature), 0
@@ -159,9 +168,10 @@ def usefuladj(feature, featcnt, adject, adjcnt, frstfreq):
             j += 1
     for i in range(ll):
         tmp[i] = tmp[i]*adjcnt[i]
-    #print(adjcnt)
-    #print(tmp)
+    # print(adjcnt)
+    # print(tmp)
     return tmp
+
 
 def foreachfeat(feature, featcnt, frstfreq):
     tmp = []
@@ -191,15 +201,17 @@ def foreachfeat(feature, featcnt, frstfreq):
 
 #############################################
 '''TOKENIZATION AND NOUNS'''
-#review = "thiss a sent tokenize test. this is sent two. is this sent three? sent 4 is cool and picture is bad! Now its your turn."
-#review = "Superb moto Builed Moto g5s plus which I loved before buying now too it's just like what I wanted, processing is gud no heating things gone up till now using it from 8 days."
-review="If your are going to buy this laptop, think twice before buying it. I have been using this laptop for 2 years. these things i like about it and don't pros- U get good specs at this price. Keyboard back light is amazing. Your batter gonna perform awesome for a year may be ( depends on usage - i used to write code and graphic processing. Cons- Worst screen -- It feels like i'm looking at 1997 tv screens after 3-4 months later i can clearly see screen blinks red and white lines going up and down - twice i have changed screen - screen cost around 5k to 6k .Heating issues. The palm rest around touch pad catches durt so quickly so quickly that u have to keep a cloth with u all the time. Over all Build quality is sooo poor .. really buy acer or asus or macbook their build quality is just amazing."
-review=review.lower()
+# review = "Amazon web services or AWS is the cutting edge public cloud provider and this is the best public cloud service I have ever seen. We are using AWS for a very long time and really like the overall features and output, using this cloud we can not run our web application as well the mobile application seamlessly. AWS provides many options related to the servers and instances, I can choose EC2, RDS, Lamda, Redshift, and many more, these all are great and works seamlessly. I can also opt for the on-demand servers which are costly but only I need to pay when I use these servers, on another hand, I have the option to go for the reserve servers which saves my cost but I need to make some volume and time commitment to the AWS. There are many other features like I can opt for the savings plan or spot instances which are really good. The integration of this platform is good but seamless, though I get great technical support from AWS all the time but the cost of business support and the premium support is very costly. The dashboard or the console is very easy to use and I can see all my usage and reports, I can see the recommendation to reduce the cost, and also I have all the visibility in this dashboard. The servers are auto-scaled up and down and this is great. AWS helps us to run seamlessly web application and provide great business services. hence my overall experience using AWS is very good. "
+# review += "Amazon Web Services or AWS is the best public cloud at this recent time. We at my current organization use AWS for all our cloud-related needs and really like the overall flexibility of this public cloud platform. I can opt for the on-demand servers, I can go for the reserve-like pricing or simply I can go for the saving plans, AWS provides all the flexibility to its users. There is a feature in which I need to pay only for those servers which I am using and if I am not using that server I don't need to pay anything to them, this is really a great feature. The user interface is very good of this platform and I can see and view all the reports/usages pattern using the dashboard and even I can export the usage report using the dashboard. The technical support is amazing though there are very heavy price we need to pay for the business support but the service is very good. The cost of this public cloud is very high but I can reduce the cost using the reserve feature of EDP feature using these features I can get heavy discounts. AWS also has many business services and infrastructure service which is very easy to use and effective as well, I can use the S3 service for the emails and also servers for the emails, I can use this for the data transfer as well, this public cloud platform really works great and fast, the security of this cloud is amazing, I don't have to worry about the security of all our data. Hence my overall experience using this cloud is very good. "
+# review += "We have started using Amazon web services from the year of 2021 July to create the Healthcare Product. Before July 2021, All our applications for PI was in On premise server right from Database, Online, Batch services and other Data ware house services. Healthcare customer was planning to leverage all the FWA applications to sell as services which implies this can be used by other small scale customer. In order to have time to market and better service capabilities, We have planned to go with Amazon web services to host of Spaces/Servers to EC2, Database to Dynamo DB from Mongo DB, Queue services to SQS, Triggers to API gate way and drive space to S3 Object store to store all our data/imaging repositories enablement. Currently our product is in Business testing with better approach of all services in to one shape with less time to market for integration/release and deployment. "
+# review += "Amazon Web Service is by far the most reliable yet economical cloud platform that has a plethora of options for individuals, and enterprises alike. It is highly scalable and the modular architecture ensures that work doesn't suffer or stop because a particular service was not opted for at the beginning. All of this is backed by one of the most robust networks globally with dedicated servers and infrastructure to ensure maximum uptime, something that most modern businesses need in the web era. "
+
+review = review.lower()
 pos_review = mult_token(review)
 
 feature, featcnt = transaction(pos_review)
 feature, featcnt = rem_stop_word(feature, featcnt)
-#feature = lemm(feature)
+# feature = lemm(feature)
 
 #############################################
 
@@ -207,22 +219,22 @@ feature, featcnt = rem_stop_word(feature, featcnt)
 '''PROCESS FOR ADJECTIVES'''
 adject, adjcnt = cntadj(pos_review)
 adject, adjcnt = rem_stop_word(adject, adjcnt)
-#adject = lemm(adject)
-# print "Adjectives : ",
-# print adject, adjcnt
+# adject = lemm(adject)
+# print("Adjectives : ")
+# print(adject, adjcnt)
 
-seed_file="seed_list.csv"
+seed_file = "seed_list.csv"
 
 sentence_orient = []
 adjective_dic = {}
 
 for row in adject:
-	cl=adjective(seed_file,row)
-	cl.file_read()
-	[pos,neg, adjmap]=cl.orientation()
-	sentence_orient.append((pos, neg))
-	cl.file_write()
-	adjective_dic.update( adjmap )
+    cl = adjective(seed_file, row)
+    cl.file_read()
+    [pos, neg, adjmap] = cl.orientation()
+    sentence_orient.append((pos, neg))
+    cl.file_write()
+    adjective_dic.update(adjmap)
 #############################################
 
 #############################################
@@ -232,16 +244,16 @@ support = int((0.1)*len(feature))
 tmp = convert1d(transaction)
 lstunq = set(tmp)
 frstfreq = freqone(lstunq, tmp)
-dct, dct2 =  createdct(frstfreq)
+dct, dct2 = createdct(frstfreq)
 scndfreq = freq2(transaction, dct, dct2)
 
-# print "first frequent : ", frstfreq
-# print "second frequent : ",scndfreq
-# print scndfreq
+# print("first frequent : ", frstfreq)
+# print("second frequent : ", scndfreq)
+# print(scndfreq)
 opin = usefuladj(feature, featcnt, adject, adjcnt, frstfreq)
-feature_list  = foreachfeat(feature, featcnt, frstfreq)
-# print "frture list : ", feature_list
-#print adjmap
+feature_list = foreachfeat(feature, featcnt, frstfreq)
+# print("frture list : ", feature_list)
+# print(adjmap)
 
 ##############################################
 '''
@@ -250,109 +262,106 @@ print(str(pos_review))		#par of speech(pos) tagging for the review for each word
 print(str(feature))			#obtaining the nouns for each sentence if it contains them
 print(str(featcnt))			#obtaining frequency of nouns for each sentence
 
-print(str(adject))			#obtaining the nouns for each sentence if it contains them
+print(str(adject))			#obtaining the adjectives for each sentence if it contains them
 print(str(adjcnt))			#obtaining frequency of adjectives for each sentence
 
-print (transaction)   #nouns for aech sentence
+print (transaction)   #nouns for each sentence
 print(dct)            #dictionary (frequentfeature:key(1,2,..))
 print(dct2)           #dictionary (key(1,2,..):frequentfeature)
 print(frstfreq)       #list of frequent features
 print(scndfreq)       #list of frequent features (2 words)
 '''
-#Sentence Orientation
+# Sentence Orientation
 
-local_ft_orientaion = {}
+local_ft_orientation = {}
 backl = 2
 frontl = 6
-#print "adjective_dic : ",  adjective_dic
+# print("adjective_dic : ",  adjective_dic)
 stopw = ['and', ',', '.', 'or', '?', 'but']
 
 
-
 for sentence, features in zip(pos_review, feature_list):
-	sentence = [i[0] for i in sentence]
-	#print sentence
-	#print features
-	for feature in features:
-		#print sentence
+    sentence = [i[0] for i in sentence]
+    # print(sentence)
+    # print(features)
+    for feature in features:
+        # print(sentence)
 
-		ft_index = sentence.index(feature)
+        ft_index = sentence.index(feature)
 
-		#go backwards 2 units or stop at stopw
-		for i in range(1,backl+1):
+        # go backwards 2 units or stop at stopw
+        for i in range(1, backl+1):
 
-			ind = ft_index-i
+            ind = ft_index-i
 
-			if(ind < 0):
-				break
+            if(ind < 0):
+                break
 
+            word = sentence[ind]
+            if word in stopw:
+                break
 
-			word = sentence[ind]
-			if word in stopw:
-				break
+            if word in adjective_dic.keys():
+                local_ft_orientation[feature] = adjective_dic[word]
+                break
 
-			if word in adjective_dic.keys():
-				local_ft_orientaion[ feature ] = adjective_dic[word]
-				break
+        if feature in local_ft_orientation:
+            continue
 
+        local_ft_orientation[feature] = 0
+        # go forward 6 units , or stop at stopw
 
-		if feature in local_ft_orientaion:
-			continue
+        for i in range(1, frontl+1):
 
-		local_ft_orientaion[feature] = 0
-		#go forward 6 units , or stop at stopw
+            ind = ft_index+i
 
-		for i in range(1, frontl+1):
+            if(ind >= len(sentence)):
+                break
 
-			ind = ft_index+i
+            word = sentence[ind]
+            if word in stopw:
+                break
 
-			if(ind >= len(sentence)):
-				break
-
-			word = sentence[ind]
-			if word in stopw:
-				break
-
-			if word in adjective_dic.keys():
-				local_ft_orientaion[ feature ] = adjective_dic[word]
-				break
+            if word in adjective_dic.keys():
+                local_ft_orientation[feature] = adjective_dic[word]
+                break
 
 
-print "feature list : ", feature_list
-for i in local_ft_orientaion.keys():
-	if(local_ft_orientaion[i]!=0):
-		print i, local_ft_orientaion[i]
+print("feature list : ", feature_list)
+for i in local_ft_orientation.keys():
+    if(local_ft_orientation[i] != 0):
+        print(i, local_ft_orientation[i])
 
-# print "feature orientation : ", local_ft_orientaion
+# print("feature orientation : ", local_ft_orientation)
 
 sentence_orientation = []
 
 for index, op in enumerate(sentence_orient):
-	net_orientation = (op[0]-op[1])
-	if(net_orientation < 0):
-		sentence_orientation.append(-1)
-	elif(net_orientation > 0):
-		sentence_orientation.append(1)
-	else:
-		for feature in feature_list[index]:
-			# if feature in local_ft_orientaion:
-			net_orientation += local_ft_orientaion[feature]
-		if(net_orientation < 0):
-			sentence_orientation.append(-1)
-		elif(net_orientation > 0):
-			sentence_orientation.append(1)
-		else:
-			sentence_orientation.append(0)
+    net_orientation = (op[0]-op[1])
+    if(net_orientation < 0):
+        sentence_orientation.append(-1)
+    elif(net_orientation > 0):
+        sentence_orientation.append(1)
+    else:
+        for feature in feature_list[index]:
+            # if feature in local_ft_orientaion:
+            net_orientation += local_ft_orientation[feature]
+        if(net_orientation < 0):
+            sentence_orientation.append(-1)
+        elif(net_orientation > 0):
+            sentence_orientation.append(1)
+        else:
+            sentence_orientation.append(0)
 
 
-print "sentence_orientation : ", sentence_orientation
+print("sentence_orientation : ", sentence_orientation)
 
 orientationSum = sum(sentence_orientation)
 if orientationSum > 0:
-	review_orientation = 1
+    review_orientation = 1
 elif orientationSum < 0:
-	review_orientation = -1
+    review_orientation = -1
 else:
-	review_orientation = 0
+    review_orientation = 0
 
-print "Review Orientation : ", review_orientation
+print("Review Orientation : ", review_orientation)
